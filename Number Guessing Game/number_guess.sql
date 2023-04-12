c0b05373bd9d7b81d6a00b7c0134f73fdb9c431a
Nils van Es Ostos <80846836+Nilsvanesostos@users.noreply.github.com>
2023-04-12 12:55:44 +0200

    Create number_guess.sql

---
+++ Number Guessing Game/number_guess.sql
@@ -0,0 +1,74 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: games; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.games (
+    games_played integer,
+    best_game integer,
+    username character varying(25)
+);
+
+
+ALTER TABLE public.games OWNER TO freecodecamp;
+
+--
+-- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.games VALUES (2, 1000, NULL);
+INSERT INTO public.games VALUES (5, 24, NULL);
+INSERT INTO public.games VALUES (2, 286, NULL);
+INSERT INTO public.games VALUES (5, 284, NULL);
+INSERT INTO public.games VALUES (2, 202, 'user_1681296819315');
+INSERT INTO public.games VALUES (5, 68, 'user_1681296819316');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
